90ef55093d3f82bd2050b67688e731f4c27f6c90
Chezzzy1 <acapulcojohncedric66@gmail.com>
2026-08-21 20:20:43 +0800

    Fix App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import Login from './components/Login';
 import OAuthRedirect from './components/OAuthRedirect';
 import ProtectedRoute from './components/ProtectedRoute';
 import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/Resetpassword';
 import VerifyEmail from './components/VerifyEmail';
 import Profile from './components/Profile';
 import { useNotes } from './hooks/useNotes';
